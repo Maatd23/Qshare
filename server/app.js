2a97c64af168ae6dd3c4ba7384b547e6a6cacff5
Maatd23 <tdeswandhy18@gmail.com>
2023-07-31 19:07:17 +0700

    initial commit

---
+++ server/app.js
@@ -0,0 +1,19 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const express = require("express");
+const cors = require("cors");
+const router = require("./router/index");
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
+
+app.use(router);
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
